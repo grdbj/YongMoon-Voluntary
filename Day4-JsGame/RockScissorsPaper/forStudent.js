95cebd369af24fbed7600082df67a9ebfefbcfa1
grdbj <85789192+grdbj@users.noreply.github.com>
2023-07-27 12:38:42 +0900

    Update forStudent.js

--- Day4-JsGame/RockScissorsPaper/forStudent.js
+++ Day4-JsGame/RockScissorsPaper/forStudent.js
@@ -108,12 +108,18 @@ const changeIMG = (answer) => { // randomNumber로부터 정해진 answer, 즉 �
 const randomNumber = (n, m) => { // n부터 m까지의 랜덤한 숫자를 만드는 난수 발생 함수를 만드세요! return 타입은 INT입니다!
 	/* 
      * n에는 시작 값이 들어오고, m에는 끝 값이 들어오게 되며
-     * n~m까지의 난수를 발생시키는 random 함수를 만드세요! (Tip. 2일차 JavaScript 기초 파일의 27페이지를 확인해보세요!)
+     * n~m까지의 난수를 발생시키는 random 함수를 만드세요! (Tip. 2일차 JavaScript 기초 파일의 37페이지를 확인해보세요!)
      * return 타입은 정수 타입인 INT입니다!
      */
+    
+     return Math.floor(Math.random()*(m-n+1))+n;
+    
+    
+
 };
 
 const judge = (answer, submit) => { // 결과를 "Draw", "Lose", "Win" 형태로 도출하여 String의 형태로 return하세요!
+    let ready = document.getElementsByClassName("countdown").innerHTML;
 	/* 
      * 가위 = -1, 바위 = 0, 보자기 = 1 입니다.
      * answer에는 상대편 카드로는 -1, 0, 1 중 하나의 값이 들어옵니다.
@@ -121,4 +127,29 @@ const judge = (answer, submit) => { // 결과를 "Draw", "Lose", "Win" 형태로
      * answser와 submit의 값을 비교하여 자신이 비겼는지, 졌는지, 이겼는지 판단하는 함수를 만드세요! 
      * return 타입은 문자열인 String이며, "Draw", "Lose", "Win" 중 하나로 return되게 하면 됩니다!
      */
+    if (answer == submit) {
+         return ready = "Draw";
+    }
+    else if (answer < submit) {
+        if (submit - answer != 2) {
+            return ready = "Win";
+        }
+        else {
+            return ready = "Lose";
+        }
+    } 
+
+    
+    else if (answer > submit) {
+        if (answer - submit != -2) {
+            return ready = "Lose";
+        }
+        else {
+            return ready = "Win";
+        }
+    }
+    
+    
+
+    
 };
